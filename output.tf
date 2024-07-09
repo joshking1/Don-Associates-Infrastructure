eef2978d4ac0a0f0f414cfda05e74ea230027e59
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-09 07:06:48 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
+# Public IP Output 
 output "public_ip" {
   value = aws_instance.web_server.public_ip
 }
 
+# Loadbalancer Output
 output "load_balancer_dns" {
   value = aws_lb.external-alb.dns_name
 }
