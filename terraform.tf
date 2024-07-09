3b72f9bfb6e65257cf3bc5c32a1c8d796c4308df
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-09 05:50:01 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0.0"
+      source = "hashicorp/aws"
+      version = "5.57.0"
     }
   }
 }
