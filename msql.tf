7afff617dd2d32d4fb473aeb460beeb2da2d8616
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-09 07:27:27 -0400

    Update msql.tf

--- msql.tf
+++ msql.tf
@@ -2,13 +2,13 @@ resource "aws_db_instance" "mysql" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "8.0.35"
+  engine_version       = "5.7.31""
   instance_class       = "db.t3.micro"
   db_name              = "mydatabase"
   username             = "admin"
   password             = "password"
   multi_az             = true
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = aws_db_parameter_group.mysql.name
   skip_final_snapshot  = true
   publicly_accessible  = false
   vpc_security_group_ids = [aws_security_group.web_sg.id]
@@ -17,6 +17,17 @@ resource "aws_db_instance" "mysql" {
   tags = {
     Name = "mysql-database"
   }
+
+resource "aws_db_parameter_group" "mysql" {
+  name   = "mysql-parameter-group"
+  family = "mysql5.7"
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+}
+
 
   provisioner "local-exec" {
     command = <<-EOT
