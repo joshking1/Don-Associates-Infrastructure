9b43592012478aed7e9c946a891edb7e26590cdb
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-09 07:17:15 -0400

    Update msql.tf

--- msql.tf
+++ msql.tf
@@ -2,12 +2,12 @@ resource "aws_db_instance" "mysql" {
   allocated_storage    = 20
   storage_type         = "gp2"
   engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  db_name                 = "mydatabase"
+  engine_version       = "8.0.35"
+  instance_class       = "db.t3.micro"
+  db_name              = "mydatabase"
   username             = "admin"
   password             = "password"
-  multi_az               = true
+  multi_az             = true
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   publicly_accessible  = false
