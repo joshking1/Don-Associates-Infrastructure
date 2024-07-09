ce3ebe146012f58f765bdf5f3e769c22c6bc1867
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-09 07:03:32 -0400

    Update application-load-balancer.tf

--- application-load-balancer.tf
+++ application-load-balancer.tf
@@ -1,22 +1,21 @@
-
 # Creating External LoadBalancer
 resource "aws_lb" "external-alb" {
   name               = "external-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web_sg.id]
+  
   tags = {
     Name = "app-lb"
   }
    
   subnet_mapping {
-    subnet_id     = aws_subnet.public_1.id
+    subnet_id = aws_subnet.public_1.id
   }
 
   subnet_mapping {
-    subnet_id     = aws_subnet.public_2.id
+    subnet_id = aws_subnet.public_2.id
   }
-  
 }
 
 resource "aws_lb_target_group" "app_tg" {
@@ -36,7 +35,7 @@ resource "aws_lb_target_group" "app_tg" {
 }
 
 resource "aws_lb_listener" "app_listener" {
-  load_balancer_arn = aws_lb.app_lb.arn
+  load_balancer_arn = aws_lb.external-alb.arn
   port              = "80"
   protocol          = "HTTP"
   default_action {
@@ -50,3 +49,4 @@ resource "aws_lb_target_group_attachment" "web_instance" {
   target_id        = aws_instance.web_server.id
   port             = 80
 }
+
