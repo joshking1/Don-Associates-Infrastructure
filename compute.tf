fc8b09b99b267e0a42cb8130b1a59b0f80262df8
joshking1 <88409463+joshking1@users.noreply.github.com>
2024-07-09 17:52:29 -0400

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -19,8 +19,24 @@ data "aws_ami" "ubuntu_server" {
 
 # All instances must be ubuntu 
 
+data "aws_ami" "ubuntu_server" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web_server" {
-  ami           = "${data.aws_ami.ubuntu_server.id}"
+  ami           = data.aws_ami.ubuntu_server.id
   instance_type = var.instance_type
   key_name      = var.keyname
   subnet_id     = aws_subnet.public_1.id
@@ -30,12 +46,16 @@ resource "aws_instance" "web_server" {
     Name = "WebServer"
   }
 
-  user_data = file("configuration2.sh")
+  user_data = templatefile("${configuration2.sh.tpl", {
+    db_endpoint = aws_db_instance.mysql.endpoint
+  })
+
+  depends_on = [aws_db_instance.mysql]
 }
 
 resource "aws_security_group" "web_sg" {
   name_prefix = "web-sg-"
-  description = "Allow web traffic"
+  description = "Allow web and database traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -60,8 +80,8 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 3306
+    to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
